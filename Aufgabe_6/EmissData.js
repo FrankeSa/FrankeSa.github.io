50d6e346b4b62812f3ff1eacdcb921afb3c4d686
FrankeSa <56167839+FrankeSa@users.noreply.github.com>
2019-11-23 10:15:29 +0100

    Add files via upload

---
+++ Aufgabe_6/EmissData.js
@@ -0,0 +1,6 @@
+var fuelConsupt = 7;
+var kilometers = 30;
+var emissionPetrol = fuelConsupt * kilometers * 0.238;
+var result = "CO2-Ausstoß für Benzin: " + emissionPetrol;
+console.log(result);
+//# sourceMappingURL=EmissData.js.map
---
+++ Aufgabe_6/EmissData.ts
@@ -0,0 +1,7 @@
+var fuelConsupt:number = 7;
+var kilometers:number = 30;
+var emissionPetrol = fuelConsupt * kilometers * 0.238;
+
+var result = "CO2-Ausstoß für Benzin: " + emissionPetrol;
+console.log(result);
+

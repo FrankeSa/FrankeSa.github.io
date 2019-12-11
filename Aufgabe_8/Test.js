d9906f43caa4c543416b588daec4253214e52e10
FrankeSa <56167839+FrankeSa@users.noreply.github.com>
2019-12-11 19:13:36 +0100

    Add files via upload

---
+++ Aufgabe_8/Test.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+while (solange)
+    eins == eins;
+ist;
+{
+    console.log(2);
+}
+for (var index_1 = 0; index_1 < 10; index_1++) {
+    console.log(index_1);
+}
+//# sourceMappingURL=Test.js.map
